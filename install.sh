c39c73f4952ccd7e8b9cd08e6b993f6037a887f0
Aben <82714169+AbenOG@users.noreply.github.com>
2021-11-20 04:49:30 +0200

    Update install.sh
    added all pacman and yay installations in one line, no need to install and reinstall along the lines.

--- install.sh
+++ install.sh
@@ -68,8 +68,7 @@ fi
 
 # Now installing my needed software and some drivers, modify this according to your likings OR system..
 # Note that most bluetooth, mainstream drivers come pre-installed in most DE so if it's not installed in yours, just go ahead and add those too..
-
-software_list_pacman=(meson systemd git dbus lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader mesa lib32-vulkan-mesa-layers lib32-opencl-mesa lib32-mesa-vdpau lib32-glu vulkan-mesa-layers opencl-mesa alacritty glu qbittorrent python pavucontrol zsh ntfs-3g obs-studio vlc lutris gparted steam bitwarden kdenlive virtualbox python-pip ufw pulseaudio)
+software_list_pacman=(wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs meson systemd git dbus lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader mesa lib32-vulkan-mesa-layers lib32-opencl-mesa lib32-mesa-vdpau lib32-glu vulkan-mesa-layers opencl-mesa alacritty glu qbittorrent python pavucontrol zsh ntfs-3g obs-studio vlc lutris gparted steam bitwarden kdenlive virtualbox python-pip ufw pulseaudio)
 software_list_yay=(discord spotify sublime-text brave-bin timeshift plex-media-server)
 
 toInstall_pac=()
@@ -126,9 +125,6 @@ sudo ufw allow out 32400/udp  ##################################################
 # Titus Ultimate gaming guide - ref -> https://www.christitus.com/ultimate-linux-gaming-guide/ ~ Credits to Chris Titus
 # Enabling ACO
 echo 'RADV_PERFTEST=aco' | sudo tee -a /etc/environment
-
-# Wine dependancies
-sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris --noconfirm
 
 # Esync enable
 ulimit -Hn
